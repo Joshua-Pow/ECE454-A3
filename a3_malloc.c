15f9736dc301c0bf5366b56ff10e93114eca277c
Joshua Pow <jokesjosh@gmail.com>
2023-10-11 18:19:47 -0400

    setup lab with structs

---
+++ a3_malloc.c
@@ -0,0 +1,57 @@
+#include "a3_malloc.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+struct h_Node {
+  int STATUS;  // defines the status of the block 0 if it is free and 1 if it is
+               // blocked
+  size_t SIZE;          // defines the size of the block in bytes
+  void* c_blk;          // defines the starting address of the current block
+  void* n_blk;          // defines the starting address of the next block
+  struct h_Node* NEXT;  // points to the next h_Node containing the data for the
+                        // next block linked to this one
+};
+
+struct h_List {
+  struct h_Node* head;  // points to the first h_Node of the list
+  struct h_Node* tail;  // points to the last h_Node of the list
+};
+
+h_Node* create_h_Node(int STATUS, size_t SIZE, void* c_blk, void* n_blk) {
+  h_Node* newNode = (h_Node*)malloc(sizeof(h_Node));
+  newNode->STATUS = STATUS;
+  newNode->SIZE = SIZE;
+  newNode->c_blk = c_blk;
+  newNode->n_blk = n_blk;
+  newNode->NEXT = NULL;
+  return newNode;
+}
+
+void h_layout(h_Node* ptr) {
+  h_Node* currentNode = ptr;
+  if (ptr == NULL) {
+    printf("NULL\n");
+    return;
+  }
+  while (ptr->NEXT != NULL) {
+    printf("--------------------\n");
+    printf("STATUS: %d\n", ptr->STATUS);
+    printf("SIZE: %lu\n", ptr->SIZE);
+    printf("c_blk: %p\n", ptr->c_blk);
+    printf("n_blk: %p\n", ptr->n_blk);
+    ptr = ptr->NEXT;
+  }
+}
+
+int m_init(void) { return 0; }
+
+int main(int argc, char const* argv[]) {
+  void* c_break = sbrk(4096);
+  printf("1 - %p \n", c_break);
+  c_break = sbrk(4096);
+  printf("2 - %p \n", c_break);
+  c_break = sbrk(4096);
+  printf("3 - %p \n", c_break);
+  return 0;
+}
---
+++ a3_malloc.h
@@ -0,0 +1,56 @@
+#ifndef A3_MALLOC_H
+#define A3_MALLOC_H
+
+#include <stdio.h>
+
+typedef struct h_Node h_Node;
+typedef struct h_List h_List;
+
+void h_layout(h_Node* ptr);
+
+int m_check(void);
+
+// This method should be called to initialize the heap area in your program,
+// before calling any other methods. It returns 0 for a successful initializing
+// of the Heap and returns not 0 in case of any problems. Use this method to
+// make an initial size for the Heap segment. All memory allocation for dynamic
+// usage should be done within this part.
+int m_init(void);
+
+// This method returns a pointer to the allocated block in the Heap with size
+// bytes (at least). This is an entire separated block within the Heap and
+// should not have any overlap with the other allocated blocks. The method
+// should search to find the best fit in the Heap for that given size and
+// allocate it. In this process the memory has already been divided into various
+// size of blocks. To manage these blocks a mechanism is provided at
+// section 3.1.3 using a linked-list. Each node in this linked-list, naming
+// h_Node, is connected to a block in the Heap and contains its information.
+// Thus for tracking the information about each block, its relevant h_Node
+// should be checked
+void* m_alloc(size_t size);
+
+// This method returns the allocated block pointed to by ptr to the free space
+// of the Heap. This method should work if and only if the ptr is valid, meaning
+// that the m_free(ptr) should be called following a call to m_malloc() or
+// m_realloc() that already generated its ptr parameter. Like the real free()
+// method, the m_fee() should set the allocated block as a free block now, and
+// this should be done by modifying the information in the relevant h_Node of
+// that block. Moreover, it should also check if the previous and/or next blocks
+// are free or not, and if yes, join them to the current free block to make a
+// bigger free block and finally update all the relevant h_Nodes.
+void m_free(void* ptr);
+
+// This method returns a pointer to the new allocated block in the Heap with
+// size bytes (at least) after resizing an old block pointed to by ptr. This is
+// an entire separated block within the Heap and should not have any overlap
+// with the other allocated blocks.
+// This method changes the size of the old block pointed to by ptr. The address
+// of the new ptr could be the same as the old ptr or be different. Note that
+// the content of the common parts between the old and the new blocks should be
+// the same. For example if the old block has the size=200 bytes and the new
+// block has the size=250 bytes, this means that the first 200 bytes of both
+// blocks are the same (actually the old block will be replaced by the new one,
+// but the contents should be kept for the common size).
+void* m_realloc(void* ptr, size_t size);
+
+#endif
